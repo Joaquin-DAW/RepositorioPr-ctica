252b1f78c73ca59bd422eb7ed822734174d794b0
Joaquin-DAW <joaquin.cabezas.berjillos.al@iespoligonosur.org>
2024-03-12 13:38:51 +0100

    Segundo Commit

--- Proyecto Practica/src/Paquete1/Principal.java
+++ Proyecto Practica/src/Paquete1/Principal.java
@@ -5,5 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Tarea 2 UD4: Control de versiones");
+		
+		System.out.println("Nuevo cambio para el commit 2");
 	}
 }
