813ddafd95bdbedcf7b142f72b63b5cf2a5c9eb8
Joaquin-DAW <joaquin.cabezas.berjillos.al@iespoligonosur.org>
2024-03-12 13:37:38 +0100

    Primer Commit

---
+++ Proyecto Practica/src/Paquete1/Principal.java
@@ -0,0 +1,9 @@
+package Paquete1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Tarea 2 UD4: Control de versiones");
+	}
+}
