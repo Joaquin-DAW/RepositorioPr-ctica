05414f17eedde6a854416ad7295a77cdb3cd850c
Joaquin-DAW <joaquin.cabezas.berjillos.al@iespoligonosur.org>
2024-03-12 14:07:11 +0100

    Cambios del paquete 2

---
+++ Proyecto Practica/src/Paquete2/Secundaria.java
@@ -0,0 +1,9 @@
+package Paquete2;
+
+public class Secundaria {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Estoy en el paquete 2");
+	}
+}
